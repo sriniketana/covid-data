4109e1e7677a77081645105d9afa1961c4125253
SRIHARI KULKARNI <skulkarni@in.ibm.com>
2020-05-25 23:30:31 +0530

    Setup for kabanero stack

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-module.exports = (/*options*/) => {
-  // Use options.server to access http.Server. Example with socket.io:
-  //     const io = require('socket.io')(options.server)
-  const app = require('express')()
-  const data = require('./file.json')
+const app = require('express')()
+const data = require('./file.json')
   var passport = require('passport-mfp-token-validation').Passport;
   var mfpStrategy = require('passport-mfp-token-validation').Strategy;
 
@@ -21,20 +18,16 @@ module.exports = (/*options*/) => {
 
   app.use(passport.initialize());
 
-  app.get('/', (req, res) => {
-    // Use req.log (a `pino` instance) to log JSON:
-    req.log.info({message: 'Hello from Appsody!'});
-    res.send('Hello from Appsody!');
-  });
+app.get('/', (req, res) => {
+  res.send("Hello from Appsody!");
+});
+ 
+app.get('/getPatients', passport.authenticate('mobilefirst-strategy', {
+  session: false,
+  scope: 'accessRestricted'
+}), (req, res, next ) => {
+res.send('Covid cases'  + JSON.stringify(data) );
 
+});
 
-  app.get('/getPatients', passport.authenticate('mobilefirst-strategy', {
-        session: false,
-        scope: 'accessRestricted'
-    }), (req, res, next ) => {
-  		res.send('Covid cases'  + JSON.stringify(data) );
-
-  	});
-
-  return app;
-};
+module.exports.app = app;
