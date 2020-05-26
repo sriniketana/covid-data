af89ed0c84bde4c99d2930da8b50f23d73b2ddd0
Srihari Kulkarni <sriniketana@gmail.com>
2020-05-26 22:40:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,6 +32,6 @@ app.get('/getPatients', passport.authenticate('mobilefirst-strategy', {
 res.send('Covid cases'  + JSON.stringify(data) );
 });
 
-app.listen(3000);
+app.listen(8080);
 
 module.exports.app = app;
